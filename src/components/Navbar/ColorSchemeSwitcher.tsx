b19ef711c9819c7f48a5d8b33d10b1b959071048
Golf0ned <benjaminye.email@gmail.com>
2025-03-03 17:10:28 -0600

    fix colorschemeswitcher icon

--- src/components/Navbar/ColorSchemeSwitcher.tsx
+++ src/components/Navbar/ColorSchemeSwitcher.tsx
@@ -8,11 +8,11 @@ import {IconSun, IconMoon} from "@tabler/icons-react";
 export default function ColorSchemeSwitcher() {
     const { colorScheme, setColorScheme } = useMantineColorScheme();
     const computedColorScheme = useComputedColorScheme("dark");
-    const [icon, setIcon] = useState(<IconSun />);
+    const [icon, setIcon] = useState(computedColorScheme === "dark" ? <IconSun /> : <IconMoon />);
 
     const toggleColorScheme = () => {
-        setColorScheme(colorScheme === "dark" ? "light" : "dark");
-        setIcon(colorScheme === "dark" ? <IconMoon /> : <IconSun />);
+        setColorScheme(computedColorScheme === "dark" ? "light" : "dark");
+        setIcon(computedColorScheme === "dark" ? <IconMoon /> : <IconSun />);
     }
 
     return (
