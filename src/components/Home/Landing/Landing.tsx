17e139fb962283ed8a2ad7d1ab347e378801b7d1
Golf0ned <benjaminye.email@gmail.com>
2025-03-07 02:49:01 -0600

    resume

--- src/components/Home/Landing/Landing.tsx
+++ src/components/Home/Landing/Landing.tsx
@@ -19,12 +19,32 @@ import {
 
 const buttons = [
     [
-        { text: "Learn about me", icon: <IconMoodSmileBeam /> },
-        { text: "Check out my work", icon: <IconCode /> },
+        { 
+            text: "Learn about me",
+            icon: <IconMoodSmileBeam />,
+            href: "#about",
+            newTab: false,
+        },
+        {
+            text: "Check out my work",
+            icon: <IconCode />,
+            href: "/projects",
+            newTab: false,
+        },
     ],
     [
-        { text: "Peek at my resume", icon: <IconFileInvoice /> },
-        { text: "Get in touch", icon: <IconMail /> },
+        {
+            text: "Peek at my resume",
+            icon: <IconFileInvoice />,
+            href: "/assets/Resume_Benjamin_Ye.pdf",
+            newTab: true,
+        },
+        {
+            text: "Get in touch",
+            icon: <IconMail />,
+            href: "/contact",
+            newTab: false,
+        },
     ],
 ];
 
@@ -34,16 +54,37 @@ export default function Landing({ onComplete }) {
 
     const buttonGroups = buttons.map((group, index) => (
         <Group key={index}>
-            {group.map(({ text, icon }, index) => (
-                <Button
-                    key={`${text}-${index}`}
-                    variant="default"
-                    size="md"
-                    leftSection={icon}
-                >
-                    {text}
-                </Button>
-            ))}
+            {group.map(({ text, icon, href, newTab }, index) => {
+                if (newTab) {
+                    return (
+                        <Button
+                            key={`${text}-${index}`}
+                            variant="default"
+                            size="md"
+                            leftSection={icon}
+                            component="a"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={href}
+                        >
+                            {text}
+                        </Button>
+                    );
+                }
+                else {
+                    return (
+                        <Button
+                            key={`${text}-${index}`}
+                            variant="default"
+                            size="md"
+                            leftSection={icon}
+                            href={href}
+                        >
+                            {text}
+                        </Button>
+                    );
+                }
+            })}
         </Group>
     ));
 
