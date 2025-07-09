3989d23d944946668712ca7fa7459c6e063d173b
Benjamin Ye <benjaminye.email@gmail.com>
2025-07-08 23:24:03 -0400

    initial blog cards buildout

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,15 +1,46 @@
 import {
+    EqualHeight,
+    EqualHeightElement,
+} from "react-equal-height";
+
+import {
     Container,
+    Grid,
     Text,
     Title,
 } from "@mantine/core";
 
+import { posts } from "data/blog";
+import type { BlogPost } from "data/blog";
+
+import BlogCard from "./BlogCard";
+
 
 export default function Blog() {
+    const blogCards = posts.map((post: BlogPost) => (
+        <Grid.Col
+            key={post.slug}
+            span={{ base: 12, sm: 6, lg: 4 }}
+        >
+            <EqualHeightElement name="blog-card">
+                <BlogCard
+                    title={post.title}
+                    date={post.date}
+                    slug={post.slug}
+                />
+            </EqualHeightElement>
+        </Grid.Col>
+    ));
+
     return (
         <Container>
             <Title order={1}>Blog</Title>
             <Text>Under construction!</Text>
+            <EqualHeight>
+                <Grid>
+                    {blogCards}
+                </Grid>
+            </EqualHeight>
         </Container>
     );
 }
---
+++ src/components/Blog/BlogCard.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router";
+
+import {
+    Button,
+    Card,
+    Group,
+    Text,
+    Title,
+} from "@mantine/core";
+
+import type { BlogPost } from "data/blog";
+
+export default function BlogCard({ title, date, slug }: BlogPost) {
+    return (
+        <Card shadow="sm" withBorder h="100%">
+            <Card.Section>
+            {/* Placeholder for future image or media content */}
+            </Card.Section>
+
+            <Group
+                justify="space-between"
+                my="sm"
+            >
+                <Title order={3}>{title}</Title>
+                <Text size="sm" color="dimmed">{date}</Text>
+            </Group>
+
+            <Button
+                variant="outline"
+                component={Link}
+                to={`/blog/${slug}`}
+            >
+                Read More
+            </Button>
+        </Card>
+    );
+}
